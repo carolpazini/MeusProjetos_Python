145b7e254b71582cdf9afbf4ecced57a2486c15b
carolpazini <caroline_pazini@hotmail.com>
2022-08-10 17:35:28 -0300

    primeiro commit do python Projeto FIAP

---
+++ Capitulo2-Variaveis/Variaveis2.py
@@ -0,0 +1,6 @@
+responsavel=input("Digite do responsável: ")
+participante=input("Digite do participante: ")
+evento=input("Digite o nome do evento: ")
+valorGasto=float(input("Digite o valor gasto na entrada: "))
+
+print("Declaro para o(a) senhor(a) " + responsavel + " que o(a) senhor(a) " + participante + " esteve presente no evento " + evento + " e gastou o valor de R$ " +str(valorGasto) + " com a entrada.")
